286b57f1dcb22d4e4f7bf0199473995dbb19f8e4
Unai Famoso <unai.fam@gmail.com>
2025-09-03 09:10:50 +0200

    apps gradio render

--- app/app_render_dispute.py
+++ app/app_render_dispute.py
@@ -13,7 +13,7 @@ import joblib
 """
 
 
-python app_render_timely.py
+python app_render_dispute.py
 
 
 
@@ -42,7 +42,11 @@ model_path = os.path.join(base_dir,"..", "models", "modelo_timely_tree_sin_comp_
 with open(model_path, "rb") as f:
     modelo_timely = joblib.load(f)
 
+from tensorflow import keras
 
+
+model_path = os.path.join(base_dir,"..", "models", "modelo_dispute_red_def.keras")
+modelo_dispute = keras.models.load_model(model_path)
 
 def predict_timely (    
     Product:str,
@@ -69,7 +73,7 @@ def predict_timely (
             "State":[State],	
             })
 
-    display(features)
+    
     features["Product"] = product_encoder.transform(features["Product"])
     features["Sub-product"] = sub_product_encoder.transform(features["Sub-product"])
     features["Issue"] = Issue_enc.transform(features["Issue"])
@@ -77,7 +81,7 @@ def predict_timely (
     features["State"] = State_enc.transform(features["State"])
     
     
-    display(features)
+    
     pred=(float(modelo_timely.predict_proba(features)[:,1])>=0.5)
     prob= float(modelo_timely.predict_proba(features)[:,1])
 
@@ -89,20 +93,95 @@ def predict_timely (
     return {"response01": int(pred), "timeprob":prob, "response":response
             }
 
+
+
+def predict_dispute (
+    Product:str, 
+    Subproduct:str, 
+    Issue:str, 
+    Subissue:str, 
+    State:str,
+
+    Company:str,
+
+    Companyresponse:str, 
+    weekday:str,
+    timely=None
+    ):
+
+    """
+    Funcion que predice si el cliente disputara o no.
+    Si no se da timely response lo predice y da los reslutado
+    Devuelve:
+        "response":Yes/No
+        "response01": 1/0
+        "prob": %
+        timely:
+            "response":Yes/No
+            "response01": 1/0
+            "prob": %
+
+    """
+    
+
+    if timely == None:
+        timely_res = (predict_timely(Issue=Issue,Subissue=Subissue ,  Product=Product, Subproduct=Subproduct , State=State ))
+        timely=timely_res["response"]
+    else:
+        timely_res='Not calculated'
+
+    print(timely_res)
+    
+    features=pd.DataFrame({
+
+            "Product":[Product],
+            "Sub-product": [Subproduct],	
+            "Issue":[Issue],	
+            "Sub-issue":[Subissue],	
+            "State":[State],	
+            "Company":[Company],
+            "Company response":[Companyresponse],
+            "Timely response?": [timely],
+            
+            
+            
+            "weekday":[weekday],})
+    
+
+
+    features_pro=preprocesador.transform(features)
+
+    #pred=modelo_dispute.predict(features_pro)[0]
+    pred=(modelo_dispute.predict(features_pro) >= 0.5).astype(int)
+    if pred== 1:
+        res="Yes"
+    else :
+        res="No"
+    prob=modelo_dispute.predict(features_pro)
+
+    return {"response":res, "response01": int(pred), "prob": float(prob), "timely":timely_res}
+
+
+
 iface = gr.Interface(
-    #meto la fucncos
-    fn=predict_timely,
-    inputs=[# meto el front
+    fn=predict_dispute,
+    inputs=[
         gr.Dropdown(choices=product_encoder.classes_.tolist(), value='Unknown or not specified', label="Product"),
         gr.Dropdown(choices=sub_product_encoder.classes_.tolist(), value='Unknown or not specified', label="Sub-product"),
         gr.Dropdown(choices=Issue_enc.classes_.tolist(), value='Unknown or not specified', label="Issue"),
         gr.Dropdown(choices=sub_Issue_enc.classes_.tolist(), value='Unknown or not specified', label="Sub-issue"),
+        
         gr.Dropdown(choices=State_enc.classes_.tolist(), value='Unknown or not specified', label="State"),
+        gr.Dropdown(choices=Company_enc.classes_.tolist(), value='Unknown or not specified', label="Company"),
         gr.Dropdown(choices=Company_response_enc.classes_.tolist(), value='Unknown or not specified', label="Company response"),
+        gr.Dropdown(choices=week_enc.classes_.tolist(),  label="weekday"),
+        gr.Dropdown(choices=["Yes", "No", None], value=None, label="Timely response?"),
+       
+        
+        
     ],
-    #sale json
     outputs="json",
-    title="Predicción Timely",
+    title="Predicción Dispute",
     description="Introduce los parámetros para obtener la predicción."
 )
 if __name__ == "__main__":
